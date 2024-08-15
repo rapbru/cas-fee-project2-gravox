56e6a9592c1d3a131f1cf912c78c3d4e7b32349a
Raphael Brunner <raphael.brunner@ost.ch>
2024-08-15 12:54:41 +0200

    eslint errors fixed for backend

--- backend/controller/plc-controller.js
+++ backend/controller/plc-controller.js
@@ -1,15 +1,19 @@
-import { plcData } from '../services/plc-data.js'
-import { TagData } from '../services/tag-data.js';
+import PLCData from '../services/plc-data.js'
+// import { TagData } from '../services/tag-data.js';
 
 export class PlcController {
 
+    constructor(plcData) {
+        this.plcData = plcData || new PLCData();    
+    }
+
     getPlcValues = async (req, res) => {
-        res.json((await plcData.all() || []))
-    };
+        res.json((await this.plcData.all()) || []);
+    }
 
     getTagValue = async (req, res) => {
-        res.json(await plcData.get(req.params.tagName));
-    };
+        res.json(await this.plcData.get(req.params.tagName));
+    }
 
     // doSomething = async (req, res) => {
     //     res.json(await plcData.add(req.body));
