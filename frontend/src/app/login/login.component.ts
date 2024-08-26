eb07a762a59a88cf4d372d523c3d497913bc21d1
Raphael Brunner <raphael.brunner@ost.ch>
2024-08-26 14:56:31 +0200

    remove console log

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -14,7 +14,6 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
-    console.log("login");
     this.authService.login();
     this.router.navigate(['/overview']);
   }
