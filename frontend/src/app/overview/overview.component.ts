b906b5aa12481446cbc1abc5fd6487c8400694eb
Raphael Brunner <raphael.brunner@ost.ch>
2024-10-09 14:13:01 +0200

    drag and drop feature

--- frontend/src/app/overview/overview.component.ts
+++ frontend/src/app/overview/overview.component.ts
@@ -1,60 +1,88 @@
-import { Component, computed, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Position } from '../position/position.model';
 import { PositionService } from '../position/services/position.service';
 import { CommonModule } from '@angular/common';
 import { PositionComponent } from './position.component';
+import { CdkDragDrop, DragDropModule, moveItemInArray  } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
 // import { PositionEditComponent } from '../position/position-edit/position-edit.component';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
   // imports: [CommonModule, PositionComponent, PositionEditComponent],
-  imports: [CommonModule, PositionComponent],
+  imports: [CommonModule, PositionComponent, DragDropModule, MatIconModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent implements OnDestroy, OnInit {
-  positions: Position[] = [];
-  selectedPosition: Position | undefined;
-  positionService = inject(PositionService);
-
-  // constructor(private positionService: PositionService) {
-  //   this.loadPositions();
-  // }
+  // positions: Position[] = [];
+  // selectedPosition: Position | undefined;
+  private positionService = inject(PositionService);
+  public enableEdit = signal<boolean>(false);;
+  private firstHalfLength = 25;
 
   ngOnInit() {
     this.positionService.startFetching();
   }
 
-  // loadPositions() {
-  //   this.positionService.load().subscribe(positions => {
-  //     this.positions = positions;
-  //     this.halfLength = Math.ceil(this.positions.length / 2);
-  //   });
-  // }
+  positions = computed(() => {
+    return this.positionService.orderedPositions();  
+  });
 
   halfLength = computed(() => {
-    return Math.ceil(this.positionService.positions().length / 2) 
-  })
-
+    return Math.ceil(this.positionService.orderedPositions().length / 2);
+  });
 
   firstHalf = computed(() => {
-    return this.positionService.positions().slice(0, this.halfLength());
-  })
-
+    return this.positions().slice(0, this.firstHalfLength);
+  });
 
   secondHalf = computed(() => {
-    return this.positionService.positions().slice(this.halfLength());
-  })
+    return this.positions().slice(this.firstHalfLength);
+  });
 
+  drop(event: CdkDragDrop<Position[]>, list: 'firstHalf' | 'secondHalf') {
+    let movedBetween = false;
+    if (event.previousContainer === event.container) {
+      // Wenn das Element innerhalb der Liste verschoben wird
+      moveItemInArray(list === 'firstHalf' ? this.firstHalf() : this.secondHalf(), event.previousIndex, event.currentIndex);
+    } else {
+      // Wenn das Element zwischen den Listen verschoben wird
+      movedBetween = true;
+      const movedItem = list === 'firstHalf' ? this.secondHalf()[event.previousIndex] : this.firstHalf()[event.previousIndex];
+      if (list === 'firstHalf') {
+        this.secondHalf().splice(event.previousIndex, 1);
+        this.firstHalf().splice(event.currentIndex, 0, movedItem);
+      } else {
+        this.firstHalf().splice(event.previousIndex, 1);
+        this.secondHalf().splice(event.currentIndex, 0, movedItem);
+      }
+    }
+    
+    const newOrderedPositions = [...this.firstHalf(), ...this.secondHalf()];
+    this.positionService.updateOrderedPositions(newOrderedPositions);
 
-  selectPosition(position: Position) {
-    this.selectedPosition = position;
+    if (movedBetween) {
+      if (list === 'firstHalf') {
+        this.firstHalfLength = this.firstHalfLength + 1;
+      } else {
+        this.firstHalfLength = this.firstHalfLength - 1;
+      }
+    }
   }
 
-  closeEdit() {
-    this.selectedPosition = undefined;
+  toggleEdit() {
+    this.enableEdit.set(!this.enableEdit());
   }
+
+  // selectPosition(position: Position) {
+  //   this.selectedPosition = position;
+  // }
+
+  // closeEdit() {
+  //   this.selectedPosition = undefined;
+  // }
 
   ngOnDestroy() {
     this.positionService.stopFetching();
