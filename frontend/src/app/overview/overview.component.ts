58a01192a30be7e0965392f0884710e48419aa7a
Raphael Brunner <raphael.brunner@ost.ch>
2024-10-10 11:25:12 +0200

    bigger min width

--- frontend/src/app/overview/overview.component.ts
+++ frontend/src/app/overview/overview.component.ts
@@ -39,7 +39,7 @@ export class OverviewComponent implements OnDestroy, OnInit {
   private updateColumnDistribution() {
     const windowWidth = window.innerWidth;
 
-    this.maxColumnCount = Math.min(Math.floor(windowWidth / 400), 10);
+    this.maxColumnCount = Math.min(Math.floor(windowWidth / 500), 10);
 
     const distribution = []; 
     for (let i = 0; i < this.columnCount; i++) {
