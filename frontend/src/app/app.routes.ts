fb469075a0d6de3cbc78064fecef96ba235ede68
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-11-03 16:32:48 +0100

    Article view: Fixed text not showing up for testing desktop view detection. (Mobile still not working).

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { OverviewComponent } from './overview/overview.component';
-import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 import { AuthGuard } from './authentication/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Standardroute zum Login
     { path: 'login', component: LoginComponent },
     { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
-    { path: 'articles', component: ArticleComponent, canActivate: [AuthGuard] },
+    { path: 'articles', component: ArticlesComponent, canActivate: [AuthGuard] },
     // Wildcard für nicht definierte Routen
     { path: '**', redirectTo: '/login' }
 ];
