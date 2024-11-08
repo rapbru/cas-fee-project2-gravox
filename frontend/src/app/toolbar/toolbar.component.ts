0f9af61f3ab5c4648e9b841af531779dd28e91c3
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-11-08 12:27:28 +0100

    Created ToolbarComponent

---
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
