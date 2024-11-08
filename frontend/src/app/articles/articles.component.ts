d51f5f94636aa3b178257de91d1b80de3fadf82e
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-11-08 11:37:26 +0100

    Removed shared ScreenService because of unresolvable errors.

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ScreenService } from 'src/app/shared/screen.service';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-articles',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'] // Corrected from styleUrl to styleUrls
 })
 export class ArticlesComponent implements OnInit {
-  isMobile: boolean = false;
-
-  constructor(private screenService: ScreenService) {}
+  isMobile = false;
 
   ngOnInit() {
-    this.screenService.isMobile$.subscribe((isMobile: boolean) => {
-      this.isMobile = isMobile;
-      console.log('isMobile (ScreenService):', this.isMobile);
-    });
+    this.checkMobile();
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkMobile();
+  }
+
+  private checkMobile() {
+    this.isMobile = window.matchMedia('(max-width: 600px)').matches;
+    console.log('isMobile (matchMedia):', this.isMobile);
   }
 }
-
