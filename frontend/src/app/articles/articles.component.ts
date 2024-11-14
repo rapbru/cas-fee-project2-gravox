75fd269a71c4e7c49f08feaad9e3f3446135262f
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-11-14 16:36:39 +0100

    First attempt at dynamic displaying of articles content based on .json / data binding.

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,36 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { DeviceDetectionService } from '../services/device-detection.service';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
+interface Article {
+  number: string;
+  customer: string;
+  area: string;
+  drainage: string;
+  anodic: string;
+  createdBy: string;
+  createdDate: string;
+  modifiedBy: string;
+  modifiedDate: string;
+  note: string;
+}
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [CommonModule, ToolbarComponent, MatCardModule],
+  imports: [CommonModule, ToolbarComponent, MatCardModule, HttpClientModule],
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  article: Article | null = null;
   isMobile = this.deviceDetectionService.isMobileSignal;
-  // isMobile = false;
 
-  constructor(private deviceDetectionService: DeviceDetectionService) {}
+  constructor(
+    private deviceDetectionService: DeviceDetectionService,
+    private http: HttpClient
+  ) {}
 
-  // ngOnInit() {
-  //   this.checkMobile();
-  // }
-
-  // @HostListener('window:resize', [])
-  // onResize() {
-  //   this.checkMobile();
-  // }
-
-  // private checkMobile() {
-  //   this.isMobile = window.matchMedia('(max-width: 600px)').matches;
-  //   console.log('isMobile (matchMedia):', this.isMobile);
-  // }
+  ngOnInit(): void {
+    this.http.get<{ article: Article }>('/assets/data.json').subscribe((data) => {
+      this.article = data.article;
+    });
+  }
 }
-
-
