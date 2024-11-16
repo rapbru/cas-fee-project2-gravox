c91d3e178196ad9e54ae69bbf259d0d7c57e0dd0
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-11-16 15:14:04 +0100

    Adjsutement to .json / component interface.

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -44,6 +44,20 @@ export class ArticlesComponent implements OnInit {
   ngOnInit(): void {
     this.http.get<{ article: Article }>('/assets/articles-data.json').subscribe((data) => {
       this.article = data.article;
+
+      if (this.article) {
+        const articleId = this.article.id;
+        this.article.number.id = `article-number-${articleId}`;
+        this.article.customer.id = `article-customer-${articleId}`;
+        this.article.area.id = `article-area-${articleId}`;
+        this.article.drainage.id = `article-drainage-${articleId}`;
+        this.article.anodic.id = `article-anodic-${articleId}`;
+        this.article.createdBy.id = `article-createdBy-${articleId}`;
+        this.article.createdDate.id = `article-createdDate-${articleId}`;
+        this.article.modifiedBy.id = `article-modifiedBy-${articleId}`;
+        this.article.modifiedDate.id = `article-modifiedDate-${articleId}`;
+        this.article.note.id = `article-note-${articleId}`;
+      }
     });
   }
 
