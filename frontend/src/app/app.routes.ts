1a2d0fdafffe4febb71651b712a7850fd9dc64b1
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-04 14:01:58 +0100

    Route from "Add Article" button in toolbar to new component "Add-Articles".

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { OverviewComponent } from './overview/overview.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { AddArticleComponent } from './add-article/add-article.component';
 import { AuthGuard } from './authentication/auth.guard';
 
+
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Standardroute zum Login
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
     { path: 'articles', component: ArticlesComponent, canActivate: [AuthGuard] },
-    // Wildcard für nicht definierte Routen
-    { path: '**', redirectTo: '/login' }
+  { path: 'add-article', component: AddArticleComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/login' }
 ];
--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,9 +8,14 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  constructor(private router: Router) {}
+
+  navigateToAddArticle() {
+    this.router.navigate(['/add-article']);
+  }
 }
