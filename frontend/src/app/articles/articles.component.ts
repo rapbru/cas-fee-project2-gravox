326823d60d09d0ed383bbba8b8ed81337a04ea15
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-04 14:01:58 +0100

    Route from "Add Article" button in toolbar to new component "Add-Articles".

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -4,10 +4,6 @@ import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { DeviceDetectionService } from '../services/device-detection.service';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClient } from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { AddArticleComponent } from './add-article/add-article.component';
-// import { HttpClientModule } from '@angular/common/http';
 
 interface ArticleField {
   id: string;
