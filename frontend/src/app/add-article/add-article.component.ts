7bfcc0058ceab3ed52dd53838c35168a9d245bd0
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-04 15:27:42 +0100

    Added buttons in "Add-Article"-Component, removed further material ui styles.

--- frontend/src/app/add-article/add-article.component.ts
+++ frontend/src/app/add-article/add-article.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatMiniFabButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-add-article',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatMiniFabButton,
+    MatTooltip
+  ],
   templateUrl: './add-article.component.html',
   styleUrl: './add-article.component.scss'
 })
