ef916d73cf2a2d30b8f5fb2ccabdc6cd69490f76
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-04 14:39:53 +0100

    Removed material ui class from body of index.html and removed further mat ui mentions.

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -8,7 +7,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
