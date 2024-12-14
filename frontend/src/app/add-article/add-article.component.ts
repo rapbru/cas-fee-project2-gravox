ef364ceb238b94bb10abbd47bf6df58ff53a1cd9
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-14 19:00:34 +0100

    Added and integrated global keypress service.

--- frontend/src/app/add-article/add-article.component.ts
+++ frontend/src/app/add-article/add-article.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {MatMiniFabButton} from "@angular/material/button";
 import {MatTooltip} from "@angular/material/tooltip";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import { AudioService } from '../audio.service';
+import { KeyEventService } from '../key-event.service';
+import { Subscription } from 'rxjs';
+
 
 
 @Component({
@@ -15,18 +18,46 @@ import { AudioService } from '../audio.service';
     MatMiniFabButton,
     MatTooltip,
     FormsModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './add-article.component.html',
   styleUrl: './add-article.component.scss'
 })
 
-export class AddArticleComponent {
+export class AddArticleComponent implements OnInit, OnDestroy {
   inputValueNameArticle = '';
   isFocused = false;
   flashCounter = false;
 
-  constructor(private audioService: AudioService) {}
+  constructor(
+    private audioService: AudioService,
+    private keyEventService: KeyEventService
+  ) {}
+
+  private keyPressSubscription: Subscription | undefined;
+
+
+  ngOnInit(): void {
+    this.keyPressSubscription = this.keyEventService.keyPressed.subscribe((event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        this.onEscapeKey();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.keyPressSubscription) {
+      this.keyPressSubscription.unsubscribe();
+    }
+  }
+
+
+  protected onEscapeKey(): void {
+    if (!this.isFocused) {
+      console.log('Escape key pressed');
+      return;
+    }
+  }
 
   onInputChange() {
     const inputLength = this.inputValueNameArticle?.length || 0;
