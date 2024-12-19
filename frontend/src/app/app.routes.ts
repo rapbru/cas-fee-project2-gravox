94a92ef26ea54f8181746bcc3c557a42f073c2b6
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-19 11:01:06 +0100

    Routing for new articles-details-component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { OverviewComponent } from './overview/overview.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { AddArticleComponent } from './add-article/add-article.component';
+import { ArticlesDetailsComponent } from './articles-details/articles-details.component';
 import { AuthGuard } from './authentication/auth.guard';
 
 
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
     { path: 'articles', component: ArticlesComponent, canActivate: [AuthGuard] },
   { path: 'add-article', component: AddArticleComponent, canActivate: [AuthGuard] },
+  { path: 'articles/:id', component: ArticlesDetailsComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/login' }
 ];
