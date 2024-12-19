d69f820e00a05992444665fb47cc9f6cf4a925f8
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-19 10:32:53 +0100

    New component article-details for setting up positions and loading article.

---
+++ frontend/src/app/articles-details/articles-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './articles-details.component.html',
+  styleUrl: './articles-details.component.scss'
+})
+export class ArticlesDetailsComponent {
+
+}
