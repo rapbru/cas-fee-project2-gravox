a9ec19084f155460f774fcf44a2226b90fab0fc6
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-23 12:04:03 +0100

    Displaying article details at top of the page.

--- frontend/src/app/articles-details/articles-details.component.ts
+++ frontend/src/app/articles-details/articles-details.component.ts
@@ -1,40 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Article } from '../models/article.model';
 import { environment } from '../../environments/environment';
+import { Article } from '../models/article.model';
 import { LoggerService } from '../services/logger.service';
 
 @Component({
   selector: 'app-articles-details',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatCardModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './articles-details.component.html',
   styleUrls: ['./articles-details.component.scss']
 })
 export class ArticlesDetailsComponent implements OnInit {
-
-  articleId: string | null = null;
-  article: Article | null = null;
   private enableLogging = environment.enableLogging;
+  article?: Article;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private loggerService: LoggerService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private loggerService: LoggerService
+  ) {}
 
-  ngOnInit(): void {
-    this.articleId = this.route.snapshot.paramMap.get('id');
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.loadArticle(id);
+      }
+    });
 
-    if (this.articleId) {
-      this.loadArticle(this.articleId);
-    }
+    // Scroll to top when component initializes
+    window.scrollTo(0, 0);
   }
 
-  loadArticle(id: string) {
-    this.http.get<Article>(`${environment.apiUrl}/articles/${id}`)
+  private loadArticle(id: string) {
+    this.http.get<Article>(`${environment.apiUrl}/article/${id}`)
       .subscribe({
         next: (article) => {
           this.article = article;
