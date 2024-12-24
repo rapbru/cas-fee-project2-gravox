ae185954ece41d312ca488e0bb6971bbbdf7d4f0
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-12-24 11:49:34 +0100

    Created component for article-card.

---
+++ frontend/src/app/article-card/article-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { Article } from '../models/article.model';
+
+@Component({
+  selector: 'app-article-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule
+  ],
+  templateUrl: './article-card.component.html',
+  styleUrls: ['./article-card.component.scss']
+})
+export class ArticleCardComponent {
+  @Input() article!: Article;
+  @Input() showCheckbox = false;
+  @Input() showHeader = true;
+  @Output() cardClick = new EventEmitter<number>();
+  @Output() loadClick = new EventEmitter<void>();
+
+  onCardClick() {
+    if (this.article.id) {
+      this.cardClick.emit(this.article.id);
+    }
+  }
+}
