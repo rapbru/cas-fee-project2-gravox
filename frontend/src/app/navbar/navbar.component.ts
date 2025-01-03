c42bc58c48da521a3fab6eb4b7166517d690e1c5
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2025-01-03 14:27:56 +0100

    Using deive-detection.service to hide navigation buttons in edit mode on mobile.

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../authentication/auth.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { OverviewStateService } from '../services/overview-state.service';
 import { PositionService } from '../services/position.service';
-
+import { CommonModule } from '@angular/common';
+import { DeviceDetectionService } from '../services/device-detection.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, MatIconModule, MatButtonModule],
+  imports: [CommonModule, RouterLink, MatIconModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   public readonly enableEdit = this.overviewStateService.enableEdit;
+  public readonly shouldShowNavigationButtons = computed(() => 
+    !this.enableEdit() || !this.deviceDetectionService.isMobileSignal()
+  );
   
-  constructor(private authService: AuthService, private router: Router, private overviewStateService: OverviewStateService, private positionService: PositionService) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private overviewStateService: OverviewStateService, 
+    private positionService: PositionService,
+    private deviceDetectionService: DeviceDetectionService
+  ) {}
 
   logout() {
+    if (this.enableEdit()) {
+      this.cancelChanges();
+    }
     this.authService.logout();
     this.router.navigate(['/login']);
   }
@@ -46,6 +59,9 @@ export class NavbarComponent {
   }
 
   navigateTo(path: string): void {
+    if (this.enableEdit()) {
+      this.cancelChanges();
+    }
     this.router.navigate([path]);
   }
 }
