50fb7d663b66cfad72fd67dbee0f8987e2c32804
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2025-01-08 17:20:50 +0100

    Added some generative styling to positions as a starting point.

--- frontend/src/app/models/position.model.ts
+++ frontend/src/app/models/position.model.ts
@@ -9,5 +9,10 @@ export interface Position {
   temperature: { actual: number; preset: number; isPresent: boolean; };
   current: { actual: number; preset: number; isPresent: boolean; };
   voltage: { actual: number; preset: number; isPresent: boolean; };
+  hasTemperature?: boolean;
+  hasCurrent?: boolean;
+  hasVoltage?: boolean;
+  presetCurrent?: number;
+  presetVoltage?: number;
   isSelected?: boolean;
 }
