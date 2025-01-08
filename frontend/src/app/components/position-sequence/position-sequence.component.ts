fdf8f8eb0de4a0e25ed9c210c21cf07e2efc4e82
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2025-01-08 18:09:49 +0100

    Removed unnecessary values from position card and added + button to add positions

--- frontend/src/app/components/position-sequence/position-sequence.component.ts
+++ frontend/src/app/components/position-sequence/position-sequence.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { Position } from '../../models/position.model';
 import { PositionService } from '../../services/position.service';
 import { LoggerService } from '../../services/logger.service';
@@ -7,7 +8,10 @@ import { LoggerService } from '../../services/logger.service';
 @Component({
   selector: 'app-position-sequence',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './position-sequence.component.html',
   styleUrls: ['./position-sequence.component.scss']
 })
