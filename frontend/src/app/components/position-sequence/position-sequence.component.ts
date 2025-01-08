c128436a14b01d5c4cefd92cb524e520ff12b7ea
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2025-01-08 18:32:11 +0100

    Sidebar / bottom sheet for selecting positions to be added.

--- frontend/src/app/components/position-sequence/position-sequence.component.ts
+++ frontend/src/app/components/position-sequence/position-sequence.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBottomSheetModule, MatBottomSheet } from '@angular/material/bottom-sheet';
 import { Position } from '../../models/position.model';
 import { PositionService } from '../../services/position.service';
 import { LoggerService } from '../../services/logger.service';
@@ -10,18 +11,33 @@ import { LoggerService } from '../../services/logger.service';
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatBottomSheetModule
   ],
   templateUrl: './position-sequence.component.html',
   styleUrls: ['./position-sequence.component.scss']
 })
 export class PositionSequenceComponent {
   selectedPositions: Position[] = [];
+  isMobile: boolean = false;
+  showPositionSelector: boolean = false;
 
   constructor(
     public positionService: PositionService,
-    private logger: LoggerService
-  ) {}
+    private logger: LoggerService,
+    private bottomSheet: MatBottomSheet
+  ) {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+  }
 
   ngOnInit() {
     this.logger.log('PositionSequenceComponent initialized');
@@ -30,6 +46,10 @@ export class PositionSequenceComponent {
 
   ngOnDestroy() {
     this.positionService.stopFetching();
+  }
+
+  togglePositionSelector() {
+    this.showPositionSelector = !this.showPositionSelector;
   }
 
   onAddPosition(position: Position) {
